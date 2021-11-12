fgeller/kt

replaces static env var names with references.

main.go
@@ -19,7 +19,7 @@ Build %v from %v.`, usageMessage, buildVersion, buildTime)
 	}
 }
 
-var usageMessage = `kt is a tool for Kafka.
+var usageMessage = fmt.Sprintf(`kt is a tool for Kafka.
 
 Usage:
 
@@ -39,9 +39,9 @@ Authorization:
 
 Authorization with Kafka can be configured via a JSON file.
 You can set the file name via an "-auth" flag to each command or
-set it via the environment variable KT_AUTH.
+set it via the environment variable %s.
 
-More at https://github.com/fgeller/kt`
+More at https://github.com/fgeller/kt`, ENV_AUTH)
 
 func parseArgs() command {
 	if len(os.Args) < 2 {

topic_test.go
@@ -10,7 +10,7 @@ func TestTopicParseArgs(t *testing.T) {
 	target := &topicCmd{}
 	givenBroker := "hans:9092"
 	expectedBrokers := []string{givenBroker}
-	os.Setenv("KT_BROKERS", givenBroker)
+	os.Setenv(ENV_BROKERS, givenBroker)
 
 	target.parseArgs([]string{})
 	if !reflect.DeepEqual(target.brokers, expectedBrokers) {
@@ -22,7 +22,7 @@ func TestTopicParseArgs(t *testing.T) {
 		return
 	}
 
-	os.Setenv("KT_BROKERS", "")
+	os.Setenv(ENV_BROKERS, "")
 	expectedBrokers = []string{"localhost:9092"}
 
 	target.parseArgs([]string{})
@@ -35,7 +35,7 @@ func TestTopicParseArgs(t *testing.T) {
 		return
 	}
 
-	os.Setenv("KT_BROKERS", "BLABB")
+	os.Setenv(ENV_BROKERS, "BLABB")
 	expectedBrokers = []string{givenBroker}
 
 	target.parseArgs([]string{"-brokers", givenBroker})

rogpeppe/hkt

Co-Authored-By: Enrique J. Hernández <enrique@heetch.com>

main.go
@@ -12,7 +12,7 @@ type command interface {
 	run(args []string) error
 }
 
-var usageMessage = `hkt is a tool for Kafka.
+var usageMessage = fmt.Sprintf(`hkt is a tool for Kafka.
 
 Usage:
 
@@ -32,10 +32,10 @@ Authorization:
 
 Authorization with Kafka can be configured via a JSON file.
 You can set the file name via an "-auth" flag to each command or
-set it via the environment variable KT_AUTH.
+set it via the environment variable %s.
 
 More at https://github.com/heetch/hkt
-`
+`, ENV_AUTH)
 
 var commands = map[string]command{
 	"consume": &consumeCmd{},

topic_test.go
@@ -10,7 +10,7 @@ func TestTopicParseArgsUsesEnvVar(t *testing.T) {
 	c := qt.New(t)
 	defer c.Done()
 
-	c.Setenv("KT_BROKERS", "hans:2000")
+	c.Setenv(ENV_BROKERS, "hans:2000")
 
 	cmd0, _, err := parseCmd("hkt", "topic")
 	c.Assert(err, qt.Equals, nil)
@@ -23,7 +23,7 @@ func TestTopicParseArgsDefault(t *testing.T) {
 	c := qt.New(t)
 	defer c.Done()
 
-	c.Setenv("KT_BROKERS", "")
+	c.Setenv(ENV_BROKERS, "")
 
 	cmd0, _, err := parseCmd("hkt", "topic")
 	c.Assert(err, qt.Equals, nil)
@@ -36,7 +36,7 @@ func TestTopicParseArgsFlagsOverrideEnv(t *testing.T) {
 	defer c.Done()
 
 	// command line arg wins
-	c.Setenv("KT_BROKERS", "BLABB")
+	c.Setenv(ENV_BROKERS, "BLABB")
 
 	cmd0, _, err := parseCmd("hkt", "topic", "-brokers", "hans:2000")
 	c.Assert(err, qt.Equals, nil)